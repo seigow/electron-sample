aa2e5160d901c29a41d8e787c39a90ee7052cb1e
seigow <seigow.fukushima@gmail.com>
2016-05-20 01:24:19 +0900

    fix Menu

--- main.js
+++ main.js
@@ -121,7 +121,7 @@ let template = [{
   }]
 }]
 
-function addUpdateMenuItems (items, position) {
+function addUpdateMenuItems(items, position) {
   const version = electron.app.getVersion()
   let updateItems = [{
     label: `Version ${version}`,
@@ -231,7 +231,11 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', function(){
+  createWindow();
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
